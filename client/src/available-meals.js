899fff893e781c1145404f4eecbebd37dcf61e1c
Supachai Ruknuy <supachai5028@gmail.com>
2024-05-23 01:39:37 -0400

    added menu

---
+++ client/src/available-meals.js
@@ -0,0 +1,84 @@
+export const MENU = [
+  {
+    id: 1,
+    name: "Pad Thai",
+    description:
+      "Stir-fried rice noodles with eggs, tofu, shrimp or chicken, bean sprouts, peanuts, and a tangy sauce.",
+    price: 10.99,
+    image:
+      "https://res.cloudinary.com/dwsihr9yg/image/upload/v1713994843/foodImages/pad-Thai_tblylb.jpg",
+  },
+  {
+    id: 2,
+    name: "Tom Yum Goong",
+    description:
+      "Spicy and sour shrimp soup flavored with lemongrass, lime leaves, chili peppers, and fish sauce.",
+    price: 9.99,
+    image:
+      "https://res.cloudinary.com/dwsihr9yg/image/upload/v1713994845/foodImages/tom-yum-Goong_hngp81.jpg",
+  },
+  {
+    id: 3,
+    name: "Green Curry",
+    description:
+      "A spicy curry made with green curry paste, coconut milk, meat (usually chicken or beef), eggplant.",
+    price: 12.99,
+    image:
+      "https://res.cloudinary.com/dwsihr9yg/image/upload/v1713994840/foodImages/green-curry_upxwhh.jpg",
+  },
+  {
+    id: 4,
+    name: "Massaman Curry",
+    description:
+      "A rich and mild curry with influences from Indian and Muslim cuisines, typically made with beef or chicken.",
+    price: 11.99,
+    image:
+      "https://res.cloudinary.com/dwsihr9yg/image/upload/v1713994841/foodImages/massaman-curry_uelclj.jpg",
+  },
+  {
+    id: 5,
+    name: "Papaya Salad",
+    description:
+      "Shredded green papaya mixed with tomatoes, peanuts, lime juice, fish sauce, and chili peppers.",
+    price: 8.99,
+    image:
+      "https://res.cloudinary.com/dwsihr9yg/image/upload/v1713994843/foodImages/som-tum_ngqkqp.jpg",
+  },
+  {
+    id: 8,
+    name: "Thai Basil Stir-Fry",
+    description:
+      "A spicy stir-fry dish made with minced meat (usually chicken, pork, or beef), Thai holy basil, garlic, chili peppers, and fish sauce.",
+    price: 10.99,
+    image:
+      "https://res.cloudinary.com/dwsihr9yg/image/upload/v1713994841/foodImages/pad-kra-pao_mujszu.jpg",
+  },
+  {
+    id: 6,
+    name: "Fried Rice",
+    description:
+      "Stir-fried rice with eggs, onions, garlic, and your choice of meat or vegetables.",
+    price: 9.99,
+    image:
+      "https://res.cloudinary.com/dwsihr9yg/image/upload/v1713994841/foodImages/khao-pad_pebfox.jpg",
+  },
+
+  {
+    id: 7,
+    name: "Thai Spring Rolls",
+    description:
+      "Crispy spring rolls filled with vegetables, sometimes with meat or seafood, served with sweet chili sauce.",
+    price: 7.99,
+    image:
+      "https://res.cloudinary.com/dwsihr9yg/image/upload/v1713994844/foodImages/thai-spring-rolls_mtahjk.jpg",
+  },
+  {
+    id: 9,
+    name: "Red Curry",
+    description:
+      "A flavorful curry made with red curry paste, coconut milk, meat or tofu, bamboo shoots, Thai eggplant, and kaffir lime leaves.",
+    price: 11.49,
+    image:
+      "https://res.cloudinary.com/dwsihr9yg/image/upload/v1713994840/foodImages/gaeng-daeng_bmg7ch.jpg",
+  },
+];
